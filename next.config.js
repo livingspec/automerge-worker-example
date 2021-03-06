402bdcef7d37aba66df5e7df81ba7998ee884b4e
Nemanja Tosic <nemanja@livingspec.com>
2021-03-07 00:11:28 +0100

    Fix wasm/prod worker

--- next.config.js
+++ next.config.js
@@ -3,8 +3,13 @@ module.exports = {
         webpack5: true,
     },
     webpack(config, options) {
-        console.log(options.webpack.version);
         config.experiments = {...config.experiments, asyncWebAssembly: true};
+
+        // workers do not work properly without using fullhash.
+        // See https://github.com/webpack/webpack/issues/12676 and the related discussions
+        config.output.chunkFilename =  options.isServer
+            ? `${options.dev ? '[name]' : '[name].[fullhash]'}.js`
+            : `static/chunks/${options.dev ? '[name]' : '[name].[fullhash]'}.js`;
 
         return config;
     }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import {useEffect} from 'react';
 
 export default function Home() {
   useEffect(() => {
-    new Worker(new URL('./worker.ts', import.meta.url));
+    new Worker(new URL('../worker/worker.ts', import.meta.url));
   }, []);
 
   return <div>test</div>;
